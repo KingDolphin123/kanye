aaa8c76be86f80861fc615a17a6599312393ffb1
KingDolphin123 <35504923+KingDolphin123@users.noreply.github.com>
2021-11-21 20:15:26 -0500

    ld

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,23 +5,20 @@ import kanye1 from "../css/1.png"
 import kanye2 from "../css/2.png"
 import kanye3 from "../css/3.png"
 import kanye4 from "../css/4.png"
-// import firestore from '../firebase'
-// import {collection, getDocs, addDoc} from 'firebase/firestore/lite'
-
 
 const Home = () => {
     const [quote, setquote] = useState('');
     const [updater, setupdater] = useState(false);
-    const [container, setcontainer] = useState('container');
-    const [kanye, setkanye] = useState('kanye');
-    const [quotecol, setquotecol] = useState('quote');
-    const [create, setcreate] = useState('create');
-    const [ld, setld] = useState('lightdark');
     const [recquote, setrecquote] = useState('');
-    const [recquoteld, setrecquoteld] = useState('recquote')
-    // const [reload, setreload] = useState(false);
     const [quotearr, setquotearr] = useState([]);
-
+    const dmobject = {
+        container: `container ${(updater? 'darkcontainer': "")}`,
+        kanye: `kanye ${(updater? 'darkkanye': "")}`,
+        quotecol: `quote ${(updater? 'darkquote': "")}`,
+        create: `create ${(updater? 'darkcreate': "")}`,
+        ld: `lightdark ${(updater? 'lightdark2': "")}`,
+        recquoteld: `recquote ${(updater? 'd': "")}`
+    }
     useEffect(() => {
         newquote();
     }, [])
@@ -32,65 +29,29 @@ const Home = () => {
         if (quote.endsWith("!") || quote.endsWith("?") || quote.endsWith('.')) {
             setquote(quote)
             setquotearr(quotearr => [...quotearr,quote]);
-            // const docRef = addDoc(collection(firestore, "Kanye"), {
-            //     quote: quote
-            //   }).then(()=>{setreload(!reload)});
         }
         else {
             setquote(quote.concat("."))
             setquotearr(quotearr => [...quotearr,quote.concat(".")]);            
-            // const docRef = addDoc(collection(firestore, "Kanye"), {
-            //     quote: quote
-            //   }).then(()=>{setreload(!reload)});
         }
         setrecquote(quotearr[quotearr.length-1])
     }
-    // useEffect(()=>{
-    //     let quottes = collection(firestore, "Kanye");
-    //     getDocs(quottes).then(snapshot => {
-    //     //snapshot is an array of all the documents in the tweets
-    //     let tempquotes = []
-    //     snapshot.forEach(document => {
-    //         tempquotes.push(document.data());
-    //     });
-    //     setrecquote(tempquotes[0].quote);
-    //     });
-    // },[reload])
-    const lightdark = () => {
-        if (!updater){
-            setcontainer('container darkcontainer');
-            setkanye('kanye darkkanye');
-            setquotecol('quote darkquote');
-            setcreate('create darkcreate')
-            setld('lightdark lightdark2')
-            setrecquoteld('recquote d')
-            setupdater(!updater);
-        }
-        if (updater){
-            setcontainer('container');
-            setkanye('kanye');
-            setquotecol('quote');
-            setcreate('create')
-            setld('lightdark')
-            setrecquoteld('recquote')
-            setupdater(!updater);
-        }
-    }
+
     return (
-        <div className={container}> 
+        <div className={dmobject.container}> 
             <div className='withbutton'>
-                <p className={kanye}>Kanye West  Quotes</p>
+                <p className={dmobject.kanye}>Kanye West  Quotes</p>
                 <div className='quoteflex'>
-                    <p className={quotecol}>{quote}</p>
+                    <p className={dmobject.quotecol}>{quote}</p>
                 </div>
             </div>
             <img className = 'kanyepic one' src={kanye1} alt='kanye1'/>
             <img className = 'kanyepic two' src={kanye2} alt='kanye2'/>
             <img className = 'kanyepic three' src={kanye3} alt='kanye3'/>
             <img className = 'kanyepic four' src={kanye4} alt='kanye4'/>
-            <button onClick={newquote} className={create}>new quote</button>
-            <button onClick={lightdark} className={ld}></button>
-            <p className ={recquoteld}>Recent quote: {recquote}</p>
+            <button onClick={newquote} className={dmobject.create}>new quote</button>
+            <button onClick={() => {setupdater(!updater)}} className={dmobject.ld}></button>
+            <p className ={dmobject.recquoteld}>Recent quote: {recquote}</p>
         </div>
 
     )
